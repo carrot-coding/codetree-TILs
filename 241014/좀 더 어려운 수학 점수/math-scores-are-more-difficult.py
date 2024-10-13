c7ba93dbcfde377bfe9d166c87e5024023936cdb
carrot-coding <ey1.noh@gmail.com>
2024-10-14 00:13:42 +0900

    Title: 좀 더 어려운 수학 점수, Time: 116ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241014/좀 더 어려운 수학 점수/math-scores-are-more-difficult.py
@@ -0,0 +1,18 @@
+inp = input()
+arr = inp.split()
+a_math = int(arr[0])
+a_eng = int(arr[1])
+
+inp = input()
+arr = inp.split()
+b_math = int(arr[0])
+b_eng = int(arr[1])
+
+if a_math > b_math:
+    print("A")
+elif a_math < b_math:
+    print("B")
+elif a_eng > b_eng:
+    print("A")
+else:
+    print("B")
