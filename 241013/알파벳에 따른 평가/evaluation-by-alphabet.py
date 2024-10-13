dcc38b2b18eb7a0304ec65ae6e7a15f20aa0c576
carrot-coding <ey1.noh@gmail.com>
2024-10-13 23:22:02 +0900

    Title: 알파벳에 따른 평가, Time: 137ms, Memory: 24MB, Status: Passed - CodeTree

---
+++ 241013/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,13 @@
+score = (input())
+if score == "S":
+    print("Superior")
+elif score == "A":
+    print("Excellent")
+elif score == "B":
+    print("Good")
+elif score == "C":
+    print("Usually")
+elif score == "D":
+    print("Effort")
+else:
+    print("Failure")
