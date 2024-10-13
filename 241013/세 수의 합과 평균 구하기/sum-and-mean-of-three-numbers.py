c0434e44e30af8184dce90c28b1faa64cd0a0bb6
carrot-coding <ey1.noh@gmail.com>
2024-10-13 22:48:11 +0900

    Title: 세 수의 합과 평균 구하기, Time: 119ms, Memory: 24MB, Status: Passed - CodeTree

--- 241013/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
+++ 241013/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.py
@@ -1,6 +1,10 @@
+# 변수 선언, 입력
 inp = input()
 arr = inp.split()
 a = int(arr[0])
 b = int(arr[1])
 c = int(arr[2])
-print(f"{a+b+c}\n{(a+b+c)/3:.0f}")
+
+# 출력
+print(a + b + c)
+print((a + b + c) // 3)
