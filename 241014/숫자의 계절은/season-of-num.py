744f0fe6bbd730107906ae126a3787459626dcc7
carrot-coding <ey1.noh@gmail.com>
2024-10-14 00:07:27 +0900

    Title: 숫자의 계절은, Time: 106ms, Memory: 24MB, Status: Wrong Answer - CodeTree

---
+++ 241014/숫자의 계절은/season-of-num.py
@@ -0,0 +1,9 @@
+month = int(input())
+if 3<= month and month <=5:
+    print('Sping')
+elif 6<= month and month <=8:
+    print('Summer')
+elif 9<= month and month <=11:
+    print('Fall')
+else:
+    print('Winter')
