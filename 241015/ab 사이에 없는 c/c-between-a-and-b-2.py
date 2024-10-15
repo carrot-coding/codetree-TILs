f0ae2451571c19db67dc47d6b791a153784b2c8c
carrot-coding <ey1.noh@gmail.com>
2024-10-15 12:51:00 +0900

    Title: ab 사이에 없는 c, Time: 111ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 241015/ab 사이에 없는 c/c-between-a-and-b-2.py
+++ 241015/ab 사이에 없는 c/c-between-a-and-b-2.py
@@ -1,10 +1,12 @@
 inp = input()
 arr = inp.split()
 a, b, c = int(arr[0]), int(arr[1]), int(inp[2])
-sat = False
+sat = True
+
 for i in range(a, b+1):
     if i % c == 0:
-        sat = True
+        sat = False
+
 if sat == True:
     print("YES")
 else :
