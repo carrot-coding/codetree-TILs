6a5da995d8582e22d761cce3ad81a35650353382
carrot-coding <ey1.noh@gmail.com>
2025-01-25 23:40:10 +0900

    Title: 별표 출력하기 8, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250125/별표 출력하기 8/print-star-8.py
+++ 250125/별표 출력하기 8/print-star-8.py
@@ -1,8 +1,10 @@
-for i in range(6):
-    if i % 2 == 0:
+n = int(input())
+for i in range(n):
+    if i % 2 != 0:
         for j in range(i+1):
-            print("*",end="")
+            print("*",end=" ")
         print()
     else:
-        print("$")
+        print("*", end=' ')
+        print()
 
