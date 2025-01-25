e9267034dfd8896f1644a390c9efa22dc50e8693
carrot-coding <ey1.noh@gmail.com>
2025-01-25 23:20:52 +0900

    Title: 체크 다이아몬드, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250125/체크 다이아몬드/check-diamond.py
@@ -0,0 +1,16 @@
+n = int(input())
+for i in range(n):
+    for j in range(n-i):
+        print(" ",end='')
+    for j in range(i):
+        print("*",end=' ')
+    print()
+for i in range(n):
+    print("*",end=' ')
+print()
+for i in range(n-1,-1,-1):
+    for j in range(n-i):
+        print(" ",end='')
+    for j in range(i):
+        print("*",end=' ')
+    print()
