eeca16120bc951c9d869f5e795ef63089b8d8d73
carrot-coding <ey1.noh@gmail.com>
2025-01-26 22:22:49 +0900

    Title: 100 도달하기, Time: 47ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250126/100 도달하기/reach-100.py
@@ -0,0 +1,17 @@
+b = int(input())
+a = 1
+arr = []
+
+arr.append(a)
+arr.append(b)
+cnt = 1
+
+while True:
+    cnt += 1
+    arr.append(arr[cnt-1]+arr[cnt-2])
+    if arr[cnt]>= 100:
+        break
+print(arr)
+
+for elem in arr:
+    print(elem,end=' ')
