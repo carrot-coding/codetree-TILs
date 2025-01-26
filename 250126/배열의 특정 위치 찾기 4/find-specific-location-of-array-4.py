bee6aeac5c9cb68a41adb8edb6006f981ef78a7b
carrot-coding <ey1.noh@gmail.com>
2025-01-26 14:08:25 +0900

    Title: 배열의 특정 위치 찾기 4, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 250126/배열의 특정 위치 찾기 4/find-specific-location-of-array-4.py
@@ -0,0 +1,10 @@
+arr = list(map(int, input().split()))
+cnt = 0
+sum_val = 0
+for i in arr:
+    if i == 0:
+        break
+    elif i % 2 == 0:
+        cnt += 1
+        sum_val += i
+print(cnt, sum_val)
