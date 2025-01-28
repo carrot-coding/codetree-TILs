03723bd02df699e7d675ab1b9ee8229b83ee8fae
carrot-coding <ey1.noh@gmail.com>
2025-01-28 22:02:20 +0900

    Title: 500 근처의 수, Time: 74ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250128/500 근처의 수/near-500.py
@@ -0,0 +1,16 @@
+arr = list(map(int, input().split()))
+max_idx = 0
+min_idx = 0
+#print(arr)
+max_setting = 0
+min_setting = 1000
+
+for elem in arr :
+    if elem < 500 and elem > 0 :
+        if elem > max_setting :
+            max_setting = elem
+    print(max_setting, end=' ')
+    elif elem > 500 :
+        if elem < min_setting :
+            min_setting = elem
+            print(min_setting)
