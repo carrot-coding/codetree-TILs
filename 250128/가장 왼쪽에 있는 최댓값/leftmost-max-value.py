f82c168a2685f9731411a3af079d06bdd2c2388b
carrot-coding <ey1.noh@gmail.com>
2025-01-28 21:15:25 +0900

    Title: 가장 왼쪽에 있는 최댓값, Time: 41ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250128/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
+++ 250128/가장 왼쪽에 있는 최댓값/leftmost-max-value.py
@@ -2,11 +2,14 @@ n = int(input())
 arr = list(map(int, input().split()))
 index_for_max = n
 
-while index_for_max != 0:
+while True:
     for elem in arr[:index_for_max] :
         max_val = 0
         if elem > max_val :
             max_val = elem
     #print(max_val)
     index_for_max = arr.index(max_val)
+    if index_for_max == 0:
+        print(index_for_max + 1, end=' ')
+        break
     print(index_for_max + 1, end=' ')
