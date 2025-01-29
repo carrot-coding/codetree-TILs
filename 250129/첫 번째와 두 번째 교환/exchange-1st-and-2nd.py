efbfbc37b241fc1288e4f8243985526129c19d81
carrot-coding <ey1.noh@gmail.com>
2025-01-29 18:23:02 +0900

    Title: 첫 번째와 두 번째 교환, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
+++ 250129/첫 번째와 두 번째 교환/exchange-1st-and-2nd.py
@@ -1,9 +1,13 @@
 A = list(input())
-for i in range(3, len(A)):
-    if A[i] == A[0] :
-        A[i] = A[1]
-    elif A[i] == A[1] :
-        A[i] = A[0]
+a = A[0]
+b = A[1]
+
+for i in range(len(A)):
+    if A[i] == a :
+        A[i] = b
+    elif A[i] == b :
+        A[i] = a
+
 #print(A)
 for elem in A :
     print(elem, end='')
