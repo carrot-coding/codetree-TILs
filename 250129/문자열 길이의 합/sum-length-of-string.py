9dac2b24aa1937e3faadbdac066461422cbde863
carrot-coding <ey1.noh@gmail.com>
2025-01-29 15:37:05 +0900

    Title: 문자열 길이의 합, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/문자열 길이의 합/sum-length-of-string.py
+++ 250129/문자열 길이의 합/sum-length-of-string.py
@@ -5,7 +5,7 @@ cnt = 0
 for i in range(n) :
     input_str = input()
     length += len(input_str)
-    if input_str[0] == 1:
+    if input_str[0] == 'a':
         cnt += 1
 
 print(length, end=' ')
