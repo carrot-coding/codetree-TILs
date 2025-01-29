f7db5f974f87368797e3ccacb040a643cf38d995
carrot-coding <ey1.noh@gmail.com>
2025-01-29 15:51:29 +0900

    Title: 특정 문자로 끝나는 문자열, Time: 47ms, Memory: 16MB, Status: Passed - CodeTree

--- 250129/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
+++ 250129/특정 문자로 끝나는 문자열/string-ending-with-specific-character.py
@@ -1,9 +1,15 @@
 cnt = 0
+str_input = []
 
 for i in range(10):
-    str_input = input()
-    if str_input[-1] == 'e':
-        print(str_input)
+    str_ = input()
+    str_input.append(str_)
+
+a = input()
+
+for i in range(10) :
+    if str_input[i][-1] == a :
+        print(str_input[i])
         cnt += 1
 if cnt == 0 :
     print('None')
