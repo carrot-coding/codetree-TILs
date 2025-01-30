9685ab4a4b25056a4db002c7a310dd412ddb9781
carrot-coding <ey1.noh@gmail.com>
2025-01-30 17:54:52 +0900

    Title: 사칙연산 함수, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250130/사칙연산 함수/quadratic-operations-function.py
@@ -0,0 +1,18 @@
+a, o, c = input().split()
+a = int(a)
+c = int(c)
+
+# Write your code here!
+def sachik(a, o, c) :
+    if o == '+':
+        print(a, o, c, '=', a+c)
+    if o == '-':
+        print(a, o, c, '-', a+c)
+    if o == '/':
+        print(a, o, c, '=', a/c)
+    if o == '*':
+        print(a, o, c, '=', a*c)            
+    else :
+        print('False')
+    
+sachik(a,o,c)
