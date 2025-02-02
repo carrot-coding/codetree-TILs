d2842bca5b15d044da40766549a5841e940f66db
carrot-coding <ey1.noh@gmail.com>
2025-02-02 19:18:26 +0900

    Title: 2개씩 그룹짓기, Time: 48ms, Memory: 16MB, Status: Passed - CodeTree

--- 250202/2개씩 그룹짓기/group-of-pairs.py
+++ 250202/2개씩 그룹짓기/group-of-pairs.py
@@ -1,8 +1,9 @@
 n = int(input())
 A = list(map(int, input().split()))
 B = [0 for _ in range(n)]
+A.sort()
 for i in range(n) :
-    B[i] = A[i] + A[i-1]
+    B[i] = A[i] + A[2*n-i-1]
 b = max(B)
 print(b)
 # Write your code here!
