59b558f6c8a3e3beecff5e5da17f3332c5bd3036
carrot-coding <ey1.noh@gmail.com>
2025-02-03 15:18:18 +0900

    Title: 사는 지역, Time: 43ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250203/사는 지역/where-live.py
@@ -0,0 +1,27 @@
+n = int(input())
+name = []
+street_address = []
+region = []
+
+for _ in range(n):
+    n_i, s_i, r_i = input().split()
+    name.append(n_i)
+    street_address.append(s_i)
+    region.append(r_i)
+
+name2 = sorted(name)
+# print(name2[-1])
+
+# Write your code here!
+class Student :
+    def __init__(self, name, street, region):
+        self.name = name
+        self.street = street
+        self.region = region
+
+for i in range(n) :
+    student = Student(name[i],street_address[i],region[i])
+    if student.name == name2[-1]:
+        print(f"name : {student.name}")
+        print(f"addr : {student.street}")
+        print(f"city : {student.region}")
