bf1d33790eac24f23a3df4b6385a8f9245a3c98f
carrot-coding <ey1.noh@gmail.com>
2025-02-10 17:26:07 +0900

    Title: 최대로 겹치는 지점, Time: 73ms, Memory: 18MB, Status: Runtime Error - CodeTree

---
+++ 250210/최대로 겹치는 지점/maximum-overlapped-points.py
@@ -0,0 +1,10 @@
+n = int(input())
+arr = [0 for _ in range(100)]
+for i in range(n) :
+    x, y = tuple(map(int, input().split()))
+    #print(x,y)
+    for j in range(x,y+1):
+        arr[j] += 1
+
+max_ = max(arr)
+print(max_)
