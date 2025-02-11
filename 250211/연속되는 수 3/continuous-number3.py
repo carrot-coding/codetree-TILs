52247590606ccb7483dd0e533781969f3f9d906c
carrot-coding <ey1.noh@gmail.com>
2025-02-11 17:52:49 +0900

    Title: 연속되는 수 3, Time: 42ms, Memory: 16MB, Status: Wrong Answer - CodeTree

--- 250211/연속되는 수 3/continuous-number3.py
+++ 250211/연속되는 수 3/continuous-number3.py
@@ -5,7 +5,9 @@ cnt = 0
 max_cnt = 0
 
 for i in range(N) :
-    if i != 0 or arr[i-1]*arr[i] > 0 :
+    if i != 0 or arr[i-1]*arr[i]>0 :
         cnt += 1
         max_cnt = max(max_cnt, cnt)
+    else :
+        cnt = 0
 print(max_cnt+1)
