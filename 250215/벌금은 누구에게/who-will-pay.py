afc197918b5b2b8cfae829fdae1dad21027be4b7
carrot-coding <ey1.noh@gmail.com>
2025-02-15 22:37:15 +0900

    Title: 벌금은 누구에게, Time: 89ms, Memory: 18MB, Status: Wrong Answer - CodeTree

---
+++ 250215/벌금은 누구에게/who-will-pay.py
@@ -0,0 +1,11 @@
+N, M, K = map(int, input().split())
+student = [int(input()) for _ in range(M)]
+
+# Write your code here!
+ans = -1
+for i in range(len(student)) :
+    for j in range(i+1, len(student)) :
+        if student[i] == student[j] :
+            ans = i
+            break
+print(ans)
