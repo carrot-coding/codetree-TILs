94e05f600afee3a9832d8159320ac60fa19f8b37
carrot-coding <ey1.noh@gmail.com>
2025-02-15 23:02:41 +0900

    Title: 벌금은 누구에게, Time: 118ms, Memory: 20MB, Status: Runtime Error - CodeTree

--- 250215/벌금은 누구에게/who-will-pay.py
+++ 250215/벌금은 누구에게/who-will-pay.py
@@ -1,11 +1,29 @@
 N, M, K = map(int, input().split())
 student = [int(input()) for _ in range(M)]
-
+arr = [0 for _ in range(N+1)]
+K__ = []
+X__ = []
 # Write your code here!
 ans = -1
-for i in range(len(student)) :
-    for j in range(i+1, len(student)) :
-        if student[i] == student[j] :
-            ans = student[i]
-            break
-print(ans)
+
+for i in student :
+    arr[i] += 1
+#print(arr)
+
+for i in range(len(arr)) :
+    if arr[i] >= K :
+        K__.append(i)
+#print(K__)
+
+if len(K__) == 0 :
+    print(-1)
+elif len(K__) == 1 :
+    for i in K__ :
+        print(i)
+else :
+    for element in K__ :
+        for i in range(len(student)):
+            if element == student[i] :
+                ans = i
+        X__.append(i)
+    print(K__[min(X__)])
