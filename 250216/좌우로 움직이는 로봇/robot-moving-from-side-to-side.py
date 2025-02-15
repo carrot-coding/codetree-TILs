757c455fadfb0e5d85b7e1418ec8936d0120f999
carrot-coding <ey1.noh@gmail.com>
2025-02-16 00:15:58 +0900

    Title: 좌우로 움직이는 로봇, Time: 50ms, Memory: 24MB, Status: Wrong Answer - CodeTree

--- 250216/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
+++ 250216/좌우로 움직이는 로봇/robot-moving-from-side-to-side.py
@@ -15,19 +15,29 @@ for i in range(m):
     for i in range(int(v)) :
         location_b[time_b+1] = location_b[time_b]  + (1 if t == 'R' else -1)
         time_b += 1
-
+print(time_a, time_b)
 #print(location_a[:10])
 #print(location_b[:10])
 compare = []
-for i in range(1, time_a+1) :
-    if location_a[i] == location_b[i] :
-        compare.append('0')
-    else :
-        compare.append('X')
-for i in range(1,len(compare)):
-    if compare[i] != compare[i-1] and compare[i] == '0' :
-        print(i+1, end=' ')        
-#print(compare[:20])
+if time_a > time_b :
+    for i in range(1, time_a+1) :
+        if location_a[i] == location_b[i] :
+            compare.append('0')
+        else :
+            compare.append('X')
+    for i in range(1,len(compare)):
+        if compare[i] != compare[i-1] and compare[i] == '0' :
+            print(i+1, end=' ')   
+else :
+    for i in range(1, time_b+1) :
+        if location_a[i] == location_b[i] :
+            compare.append('0')
+        else :
+            compare.append('X')
+    for i in range(1,len(compare)):
+        if compare[i] != compare[i-1] and compare[i] == '0' :
+            print(i+1, end=' ')           
+    #print(compare[:20])
 # change = 0
 # for i in range(len(compare)-1):
 #     if compare[i] != compare[i+1] :
