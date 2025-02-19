dba30970d091f8d08c2a200e49cfc113452666e0
carrot-coding <ey1.noh@gmail.com>
2025-02-19 10:18:00 +0900

    Title: 방향에 맞춰 이동, Time: 44ms, Memory: 16MB, Status: Wrong Answer - CodeTree

---
+++ 250219/방향에 맞춰 이동/move-in-direction.py
@@ -0,0 +1,18 @@
+n = int(input())
+moves = [tuple(input().split()) for _ in range(n)]
+dir = [move[0] for move in moves]
+dist = [int(move[1]) for move in moves]
+
+x, y = 0, 0
+for i in range(n) :
+    move = moves[i]
+    if move == 'N' :
+        y += dist[i]
+    elif move == 'W' :
+        x -= dist[i]
+    elif move == 'S' :
+        y -= dist[i]
+    else :
+        x += dist[i]
+print(x)
+print(y) 
