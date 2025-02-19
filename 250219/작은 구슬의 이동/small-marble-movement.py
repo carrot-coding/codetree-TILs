8e0bc839ccf990bace72839e588600fb7e931ddc
carrot-coding <ey1.noh@gmail.com>
2025-02-19 13:13:25 +0900

    Title: 작은 구슬의 이동, Time: 50ms, Memory: 16MB, Status: Passed - CodeTree

--- 250219/작은 구슬의 이동/small-marble-movement.py
+++ 250219/작은 구슬의 이동/small-marble-movement.py
@@ -1,22 +1,25 @@
 n, t = map(int, input().split())
 r, c, d = input().split()
-r, c = int(r), int(c)
+r, c = int(r), int(c)  # 1-indexed 유지
 
 dxs, dys = [0, 1, -1, 0], [1, 0, 0, -1]
-mapper = {
-    'R': 0,
-    'D': 1,
-    'U': 2,
-    'L': 3
-}
+mapper = {'R': 0, 'D': 1, 'U': 2, 'L': 3}
 
 dir_num = mapper[d]
-def in_range(x, y) :
-    return 0 <= x and x < n and 0 <= y and y < n
 
-for _ in range(t) :
+# in_range 함수 정의 (1-indexed 방식)
+def in_range(x, y):
+    return 1 <= x <= n and 1 <= y <= n  # 1부터 N까지 유효
+
+for _ in range(t):
+    # 먼저 이동을 시도
     nx, ny = r + dxs[dir_num], c + dys[dir_num]
-    if not in_range(nx+1, ny+1) :
-        dir_num = 3 - dir_num
 
-print(nx, ny)
+    # 이동한 곳이 범위를 벗어나면 방향을 반대로 바꾸고, 이동 취소
+    if not in_range(nx, ny):
+        dir_num = 3 - dir_num  # 방향 전환
+    else:
+        r, c = nx, ny  # 범위 안이면 이동 확정
+
+# 최종 좌표 출력 (1-indexed 그대로)
+print(r, c)
