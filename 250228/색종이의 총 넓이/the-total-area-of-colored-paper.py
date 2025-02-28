289b8ff8264418af07f5882e71c6203e9cbb6b41
carrot-coding <ey1.noh@gmail.com>
2025-02-28 14:07:14 +0900

    Title: 색종이의 총 넓이, Time: 59ms, Memory: 18MB, Status: Passed - CodeTree

--- 250228/색종이의 총 넓이/the-total-area-of-colored-paper.py
+++ 250228/색종이의 총 넓이/the-total-area-of-colored-paper.py
@@ -1,14 +1,16 @@
 n = int(input())
 points = [tuple(map(int, input().split())) for _ in range(n)]
-checked = [[0 for _ in range(101)] for _ in range(101)]
+checked = [[0 for _ in range(202)] for _ in range(202)]
 
 for element in points :
-    for i in range(element[0],element[0]+8):
-        for j in range(element[1],element[1]+8):
+    a = element[0] + 100
+    b = element[1] + 100
+    for i in range(a, a+8):
+        for j in range(b, b+8):
             checked[i][j] += 1
 cnt = 0
-for i in range(101):
-    for j in range(101):
+for i in range(202):
+    for j in range(202):
         if checked[i][j] > 0 :
             cnt += 1
 print(cnt)
