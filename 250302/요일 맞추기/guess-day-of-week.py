14e816fcc68a90cd559877a51e3c9bff122eaf63
carrot-coding <ey1.noh@gmail.com>
2025-03-02 00:37:01 +0900

    Title: 요일 맞추기, Time: 49ms, Memory: 16MB, Status: Passed - CodeTree

--- 250302/요일 맞추기/guess-day-of-week.py
+++ 250302/요일 맞추기/guess-day-of-week.py
@@ -1,6 +1,6 @@
 # 변수 선언 및 입력
 a, b, c, d = tuple(map(int, input().split()))
-days = [0, 31, 30, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
+days = [0, 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31]
 week = ['Mon','Tue','Wed','Thu','Fri','Sat','Sun']
 total_a = 0
 total_b = 0
@@ -16,7 +16,7 @@ total_b += d
 
 # print(total_a)
 # print(total_b)
-
+# print((total_b - total_a)%7)
 # if total_a > total_b :
 #     print(week[(total_b - total_a)%7])
 # else : 
