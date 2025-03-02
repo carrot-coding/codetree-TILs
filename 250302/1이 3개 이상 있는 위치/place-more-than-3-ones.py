c5ab13ccc76f0b25bd1615a9698f47afaf0dbe8c
carrot-coding <ey1.noh@gmail.com>
2025-03-02 14:56:31 +0900

    Title: 1이 3개 이상 있는 위치, Time: 70ms, Memory: 17MB, Status: Wrong Answer - CodeTree

---
+++ 250302/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -0,0 +1,21 @@
+n = int(input())
+
+checked = [list(map(int,input().split())) for _ in range(n)]
+
+def in_range(x, y):
+    return 0<= x <n and 0<= y <n
+total_cnt = 0
+
+for i in range(n):
+    for j in range(n):
+        cnt = 0
+        if in_range(i-1,j) and checked[i-1][j] == 1 :
+            cnt += 1
+        if in_range(i,j-1) and checked[i][j-1] == 1 :
+            cnt += 1
+        if in_range(i+1,j) and checked[i+1][j] == 1 :
+            cnt += 1
+        if in_range(i,j+1) and checked[i][j+1] == 1 :
+            cnt += 1
+        if cnt >= 3 :
+            total_cnt += 1
