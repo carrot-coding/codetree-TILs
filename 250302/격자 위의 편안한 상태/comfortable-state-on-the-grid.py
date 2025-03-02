b41928126791b3dbe3b3fdf63e572781113acdd3
carrot-coding <ey1.noh@gmail.com>
2025-03-02 21:30:08 +0900

    Title: 격자 위의 편안한 상태, Time: 104ms, Memory: 19MB, Status: Wrong Answer - CodeTree

---
+++ 250302/격자 위의 편안한 상태/comfortable-state-on-the-grid.py
@@ -0,0 +1,23 @@
+n, m = map(int, input().split())
+points = [tuple(map(int, input().split())) for _ in range(m)]
+checked = [[0 for _ in range(n)] for _ in range(n)]
+dxs = [1, 0, -1, 0]
+dys = [0, -1, 0, 1]
+
+def in_range(x, y):
+    return 0<=x<n and 0<=y<n
+
+for i in range(m):
+    x = points[i][0]-1
+    y = points[i][1]-1
+    checked[x][y] += 1
+    cnt = 0
+    for j in range(4):
+        nx = x + dxs[j]
+        ny = y + dys[j]
+        if in_range(nx,ny) and checked[nx][ny] == 1:
+            cnt += 1
+    if cnt > 2 :
+        print(1)
+    else :
+        print(0)
